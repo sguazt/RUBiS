6838f3392f3028a12099f70c68bb822ddbc3fc8e
sguazt <marco.guazzone@gmail.com>
2013-07-12 21:52:38 +0200

    (change:minor) Added removal and creation of database in the script for initializing the database.

--- setup/db/init_db.sh
+++ setup/db/init_db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cwd=$(dirname $0)
+mysqladmin -f -uroot -prubis drop rubis
+mysqladmin -uroot -prubis create rubis
 mysql -uroot rubis <$cwd/rubis.sql
 mysql -uroot rubis <$cwd/categories.sql
 mysql -uroot rubis <$cwd/regions.sql
