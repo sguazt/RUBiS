6706e5197ca4cec0331327f04ffd4c5f8659e3f1
sguazt <marco.guazzone@gmail.com>
2013-07-18 11:21:20 +0200

    (bug-fix:minor) Fixed some typo.

--- setup/db/init_db.sh
+++ setup/db/init_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cwd=$(dirname $0)
-mysqladmin -f -uroot -prubis drop rubis
-mysqladmin -uroot -prubis create rubis
+mysqladmin -f -uroot -p drop rubis
+mysqladmin -uroot -p create rubis
 mysql -uroot rubis <$cwd/rubis.sql
 mysql -uroot rubis <$cwd/categories.sql
 mysql -uroot rubis <$cwd/regions.sql
-mysql -uroot rubis <$cwd/update_ids.sql
+#mysql -uroot rubis <$cwd/update_ids.sql
