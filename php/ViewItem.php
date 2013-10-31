a668da27a3f94f9b441e690ec5f8cc626ad8fcfd
sguazt <marco.guazzone@gmail.com>
2013-10-31 09:46:16 +0100

    [php] (bug-fix:minor) Database field 'bid' was taken from a bad result-set ('$row' instead of '$xRow').

--- php/ViewItem.php
+++ php/ViewItem.php
@@ -54,7 +54,7 @@
           $nb = $nb + $xRow["qty"];
           if ($nb > $row["quantity"])
           {
-            $maxBid = $row["bid"];
+            $maxBid = $xRow["bid"];
             break;
           }
         }
